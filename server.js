ebbbf7c0c865873206d76247599dbd84fde6c377
sukhdevkolli <sukhdevkolli9901@gmail.com>
2024-02-04 23:47:02 -0500

    commit

--- server.js
+++ server.js
@@ -6,24 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
+const budget = require("./exercise01.json");
+console.log(budget)
 
 app.get('/budget', (req, res) => {
     res.json(budget);
